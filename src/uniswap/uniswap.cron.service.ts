eb47aaf75d8de671d094e4594706519735a51ef9
Vitaliy Trach <vital24031986@gmail.com>
2023-07-20 15:44:07 +0300

    fix: uniswap cron service

--- src/uniswap/uniswap.cron.service.ts
+++ src/uniswap/uniswap.cron.service.ts
@@ -6,6 +6,9 @@ import { UniswapService } from './uniswap.service';
 
 @Injectable()
 export class UniswapCron extends CronService {
+  private readonly TASK_NAME = 'update_pools_info_uniswap';
+  private readonly APP_NAME = 'pools_updater';
+
   constructor(
     protected readonly prisma: PrismaService,
     private readonly uniswapService: UniswapService,
@@ -13,33 +16,30 @@ export class UniswapCron extends CronService {
     super(prisma);
   }
 
-  private readonly taskName = 'update_pools_info_uniswap';
-  private readonly appName = 'pools_updater';
-
   async onModuleInit() {
     await this.updatePoolsInfo();
   }
 
   @Cron(CronExpression.EVERY_30_MINUTES, { name: 'update_pools_info_uniswap' })
   async updatePoolsInfo() {
-    if (await this.isJobRunning(this.appName, this.taskName)) {
+    if (await this.isJobRunning(this.APP_NAME, this.TASK_NAME)) {
       console.log(
-        `Cron job ${this.taskName} is already running. Skipping this run.`,
+        `Cron job ${this.TASK_NAME} is already running. Skipping this run.`,
       );
       return;
     }
 
-    await this.startJob(this.appName, this.taskName);
+    await this.startJob(this.APP_NAME, this.TASK_NAME);
 
-    console.log(`Start job ${this.taskName} at ${new Date().toUTCString()}`);
+    console.log(`Start job ${this.TASK_NAME} at ${new Date().toUTCString()}`);
     try {
       await this.uniswapService.updatePoolsInfo();
-      console.log(`Job ${this.taskName} completed successfully.`);
+      console.log(`Job ${this.TASK_NAME} completed successfully.`);
     } catch (error) {
-      console.log(`Error execution Job ${this.taskName} error: ${error}`);
+      console.log(`Error execution Job ${this.TASK_NAME} error: ${error}`);
     }
 
-    await this.completeJob(this.appName, this.taskName);
-    console.log(`Job ${this.taskName} complete`);
+    await this.completeJob(this.APP_NAME, this.TASK_NAME);
+    console.log(`Job ${this.TASK_NAME} complete`);
   }
 }
